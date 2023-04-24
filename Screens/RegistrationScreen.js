0a51ac9fb82d72c900ef9728a1b75c365cc37388
Anna Tsepilova <virgo.tsav@gmail.com>
2023-04-24 20:34:02 +0300

    start codding

---
+++ Screens/RegistrationScreen.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+import DocumentPicker from "react-native-document-picker";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+export default function RegistrationScreen() {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        style={styles.image}
+        source={require("../assets/img/main-bg.png")}
+      >
+        <View>style={styles.registerForm}</View>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  registerForm: {
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+  },
+});
