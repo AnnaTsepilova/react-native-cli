8d91beca07d8ea1b97120558263583aa8c226b11
Anna Tsepilova <virgo.tsav@gmail.com>
2023-04-28 00:36:46 +0300

    refactoring imgBg/fonts

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,8 @@
 import React, { useEffect, useState, useCallback } from "react";
-import * as Font from "expo-font";
-import AppLoading from "expo-app-loading";
 
 import {
   StyleSheet,
   View,
-  ImageBackground,
   KeyboardAvoidingView,
   Platform,
   Text,
@@ -13,7 +10,6 @@ import {
   TouchableOpacity,
   Keyboard,
   TouchableWithoutFeedback,
-  Dimensions,
 } from "react-native";
 
 const initialState = {
@@ -21,17 +17,9 @@ const initialState = {
   password: "",
 };
 
-const loadFonts = async () => {
-  await Font.loadAsync({
-    RobotoRegular: require("../assets/fonts/Roboto-Regular.ttf"),
-    RobotoMedium: require("../assets/fonts/Roboto-Medium.ttf"),
-  });
-};
-
 export default function LoginScreen() {
   const [state, setState] = useState(initialState);
   const [isShownKeyboard, setIsShownKeyboard] = useState(false);
-  const [isReady, setIsReady] = useState(false);
 
   const keyboardHide = () => {
     setIsShownKeyboard(false);
@@ -40,82 +28,61 @@ export default function LoginScreen() {
     setState(initialState);
   };
 
-  if (!isReady) {
-    return (
-      <AppLoading
-        startAsync={loadFonts}
-        onFinish={() => setIsReady(true)}
-        onError={console.warn}
-      />
-    );
-  }
-
   return (
     <TouchableWithoutFeedback onPress={keyboardHide}>
-      <ImageBackground
-        source={require("../assets/img/mainBg.jpg")}
-        resizeMode="cover"
-        style={styles.background}
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
       >
-        <KeyboardAvoidingView
-          behavior={Platform.OS == "ios" ? "padding" : "height"}
+        <View
+          style={{
+            ...styles.formContainer,
+            //width: dimensions,
+            //marginBottom: isShownKeyboard ? 0 : 0,
+          }}
         >
-          <View
-            style={{
-              ...styles.formContainer,
-              //width: dimensions,
-              //marginBottom: isShownKeyboard ? 0 : 0,
-            }}
-          >
-            <View style={styles.avatarContainer}></View>
-            <Text style={styles.formTitle}>Sign In</Text>
+          <View style={styles.avatarContainer}></View>
+          <Text style={styles.formTitle}>Sign In</Text>
 
-            <View style={styles.form}>
-              <TextInput
-                style={styles.input}
-                placeholder="Email"
-                placeholderTextColor="#BDBDBD"
-                value={state.email}
-                onFocus={() => setIsShownKeyboard(true)}
-                onChangeText={(value) =>
-                  setState((prevState) => ({ ...prevState, email: value }))
-                }
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="Password"
-                placeholderTextColor="#BDBDBD"
-                value={state.password}
-                secureTextEntry={true}
-                onFocus={() => setIsShownKeyboard(true)}
-                onChangeText={(value) =>
-                  setState((prevState) => ({ ...prevState, password: value }))
-                }
-              />
-              <TouchableOpacity
-                style={styles.button}
-                activeOpacity={0.8}
-                onPress={keyboardHide}
-              >
-                <Text style={styles.btnTitle}>SIGN IN</Text>
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <Text style={styles.signUp}>Sign Up</Text>
-              </TouchableOpacity>
-            </View>
+          <View style={styles.form}>
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              placeholderTextColor="#BDBDBD"
+              value={state.email}
+              onFocus={() => setIsShownKeyboard(true)}
+              onChangeText={(value) =>
+                setState((prevState) => ({ ...prevState, email: value }))
+              }
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              placeholderTextColor="#BDBDBD"
+              value={state.password}
+              secureTextEntry={true}
+              onFocus={() => setIsShownKeyboard(true)}
+              onChangeText={(value) =>
+                setState((prevState) => ({ ...prevState, password: value }))
+              }
+            />
+            <TouchableOpacity
+              style={styles.button}
+              activeOpacity={0.8}
+              onPress={keyboardHide}
+            >
+              <Text style={styles.btnTitle}>SIGN IN</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.signUp}>Sign Up</Text>
+            </TouchableOpacity>
           </View>
-        </KeyboardAvoidingView>
-      </ImageBackground>
+        </View>
+      </KeyboardAvoidingView>
     </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    justifyContent: "flex-end",
-  },
-
   formContainer: {
     paddingBottom: 78,
     paddingHorizontal: 16,
--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -1,14 +1,8 @@
 import React, { useEffect, useState, useCallback } from "react";
-import * as Font from "expo-font";
-import AppLoading from "expo-app-loading";
-//import * as SplashScreen from "expo-splash-screen";
-
-//import DocumentPicker from "react-native-document-picker";
 
 import {
   StyleSheet,
   View,
-  ImageBackground,
   KeyboardAvoidingView,
   Platform,
   Text,
@@ -16,7 +10,6 @@ import {
   TouchableOpacity,
   Keyboard,
   TouchableWithoutFeedback,
-  Dimensions,
 } from "react-native";
 
 const initialState = {
@@ -25,22 +18,9 @@ const initialState = {
   password: "",
 };
 
-//SplashScreen.preventAutoHideAsync();
-
-const loadFonts = async () => {
-  await Font.loadAsync({
-    RobotoRegular: require("../assets/fonts/Roboto-Regular.ttf"),
-    RobotoMedium: require("../assets/fonts/Roboto-Medium.ttf"),
-  });
-};
-
 export default function RegistrationScreen() {
   const [state, setState] = useState(initialState);
   const [isShownKeyboard, setIsShownKeyboard] = useState(false);
-  const [isReady, setIsReady] = useState(false);
-  const [dimensions, setDimensions] = useState(
-    Dimensions.get("window").width - 16 * 2
-  );
 
   const keyboardHide = () => {
     setIsShownKeyboard(false);
@@ -49,143 +29,72 @@ export default function RegistrationScreen() {
     setState(initialState);
   };
 
-  // useEffect(() => {
-  //   async function prepare() {
-  //     try {
-  //       // Pre-load fonts, make any API calls you need to do here
-  //       await Font.loadAsync({
-  //         RobotoRegular: require("../assets/fonts/Roboto-Regular.ttf"),
-  //         RobotoMedium: require("../assets/fonts/Roboto-Medium.ttf"),
-  //       });
-  //     } catch (e) {
-  //       console.warn(e);
-  //     } finally {
-  //       // Tell the application to render
-  //       setIsReady(true);
-  //     }
-  //   }
-  //   prepare();
-  // }, []);
-
-  // useEffect(() => {
-  //   const onChange = () => {
-  //     const width = Dimensions.get("window").width - 16 * 2;
-  //     setDimensions(width);
-  //   };
-
-  //   Dimensions.addEventListener("change", onChange);
-  //   return () => {
-  //     Dimensions.removeEventListener("change", onChange);
-  //   };
-  // }, []);
-
-  // const onLayoutRootView = useCallback(async () => {
-  //   if (isReady) {
-  //     // This tells the splash screen to hide immediately! If we call this after
-  //     // `setAppIsReady`, then we may see a blank screen while the app is
-  //     // loading its initial state and rendering its first pixels. So instead,
-  //     // we hide the splash screen once we know the root view has already
-  //     // performed layout.
-  //     await SplashScreen.hideAsync();
-  //   }
-  // }, [isReady]);
-
-  // if (!isReady) {
-  //   return null;
-  // }
-
-  if (!isReady) {
-    return (
-      <AppLoading
-        startAsync={loadFonts}
-        onFinish={() => setIsReady(true)}
-        onError={console.warn}
-      />
-    );
-  }
-
   return (
-    // <View
-    // onLayout={onLayoutRootView}
-    // >
     <TouchableWithoutFeedback onPress={keyboardHide}>
-      <ImageBackground
-        source={require("../assets/img/mainBg.jpg")}
-        resizeMode="cover"
-        style={styles.background}
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
       >
-        <KeyboardAvoidingView
-          behavior={Platform.OS == "ios" ? "padding" : "height"}
+        <View
+          style={{
+            ...styles.formContainer,
+            //width: dimensions,
+            //marginBottom: isShownKeyboard ? 0 : 0,
+          }}
         >
-          <View
-            style={{
-              ...styles.formContainer,
-              //width: dimensions,
-              //marginBottom: isShownKeyboard ? 0 : 0,
-            }}
-          >
-            <View style={styles.avatarContainer}></View>
-            <Text style={styles.formTitle}>Registration</Text>
+          <View style={styles.avatarContainer}></View>
+          <Text style={styles.formTitle}>Registration</Text>
 
-            <View style={styles.form}>
-              {/* name */}
-              <TextInput
-                style={styles.input}
-                placeholder="Login"
-                placeholderTextColor="#BDBDBD"
-                value={state.name}
-                onFocus={() => setIsShownKeyboard(true)}
-                onChangeText={(value) =>
-                  setState((prevState) => ({ ...prevState, name: value }))
-                }
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="Email"
-                placeholderTextColor="#BDBDBD"
-                value={state.email}
-                onFocus={() => setIsShownKeyboard(true)}
-                onChangeText={(value) =>
-                  setState((prevState) => ({ ...prevState, email: value }))
-                }
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="Password"
-                placeholderTextColor="#BDBDBD"
-                value={state.password}
-                secureTextEntry={true}
-                onFocus={() => setIsShownKeyboard(true)}
-                onChangeText={(value) =>
-                  setState((prevState) => ({ ...prevState, password: value }))
-                }
-              />
-              <TouchableOpacity
-                style={styles.button}
-                activeOpacity={0.8}
-                onPress={keyboardHide}
-              >
-                <Text style={styles.btnTitle}>SIGN UP</Text>
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <Text style={styles.signIn}>Sign In</Text>
-              </TouchableOpacity>
-            </View>
+          <View style={styles.form}>
+            {/* name */}
+            <TextInput
+              style={styles.input}
+              placeholder="Login"
+              placeholderTextColor="#BDBDBD"
+              value={state.name}
+              onFocus={() => setIsShownKeyboard(true)}
+              onChangeText={(value) =>
+                setState((prevState) => ({ ...prevState, name: value }))
+              }
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              placeholderTextColor="#BDBDBD"
+              value={state.email}
+              onFocus={() => setIsShownKeyboard(true)}
+              onChangeText={(value) =>
+                setState((prevState) => ({ ...prevState, email: value }))
+              }
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              placeholderTextColor="#BDBDBD"
+              value={state.password}
+              secureTextEntry={true}
+              onFocus={() => setIsShownKeyboard(true)}
+              onChangeText={(value) =>
+                setState((prevState) => ({ ...prevState, password: value }))
+              }
+            />
+            <TouchableOpacity
+              style={styles.button}
+              activeOpacity={0.8}
+              onPress={keyboardHide}
+            >
+              <Text style={styles.btnTitle}>SIGN UP</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.signIn}>Sign In</Text>
+            </TouchableOpacity>
           </View>
-        </KeyboardAvoidingView>
-      </ImageBackground>
+        </View>
+      </KeyboardAvoidingView>
     </TouchableWithoutFeedback>
-    // </View>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    justifyContent: "flex-end",
-    //alignItems: "center",
-  },
-
   formContainer: {
     paddingBottom: 78,
     paddingHorizontal: 16,
