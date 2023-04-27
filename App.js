4d17b8a395f182b2d9716aea254c84c56463d70c
Anna Tsepilova <virgo.tsav@gmail.com>
2023-04-27 20:36:25 +0300

    hw-02

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Text } from "react-native";
 
-//import RegistrationScreen from "./screens/RegistrationScreen";
-import LoginScreen from "./screens/LoginScreen.js";
+import RegistrationScreen from "./screens/RegistrationScreen";
+//import LoginScreen from "./screens/LoginScreen.js";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <RegistrationScreen /> */}
-      <LoginScreen />
+      <RegistrationScreen />
+      {/* <LoginScreen /> */}
       <StatusBar style="auto" />
     </View>
   );
