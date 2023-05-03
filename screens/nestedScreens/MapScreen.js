83443ef0a1a577bd6d6ac95358ec6ccdb68c5a18
Anna Tsepilova <virgo.tsav@gmail.com>
2023-05-03 22:35:59 +0300

    add mapp diteils

--- screens/nestedScreens/MapScreen.js
+++ screens/nestedScreens/MapScreen.js
@@ -1,21 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 import MapView, { Marker } from "react-native-maps";
 
-const MapScreen = ({ navigation }) => {
+const MapScreen = ({ route }) => {
+  const { latitude, longitude } = route.params.location;
   return (
     <View style={styles.container}>
       <MapView
         style={{ flex: 1 }}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude,
+          longitude,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
       >
-        <Marker coordinate={{ longitude: -122.4324, latitude: 37.78825 }} />
+        <Marker coordinate={{ longitude, latitude }} />
       </MapView>
     </View>
   );
@@ -24,8 +25,6 @@ const MapScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
-    //justifyContent: "center",
   },
 });
 
